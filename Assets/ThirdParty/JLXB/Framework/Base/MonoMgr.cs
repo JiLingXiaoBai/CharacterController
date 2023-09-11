7750a9678b420a4f239414c6f52d2c28c4a8a989
JiLingXiaoBai <uiojkl3588@qq.com>
2023-09-11 15:38:57 +0800

    引入框架和Addressable

---
+++ Assets/ThirdParty/JLXB/Framework/Base/MonoMgr.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using UnityEngine.Internal;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace JLXB.Framework
+{
+    public class MonoMgr : MonoSingleton<MonoMgr>
+    {
+        private event UnityAction UpdateEvent;
+        private event UnityAction DestroyEvent;
+        private MonoBehaviour _mono;
+        private MonoMgr() { }
+        private void Awake()
+        {
+            _mono = this;
+        }
+
+        void Update()
+        {
+            if (UpdateEvent != null)
+            {
+                UpdateEvent?.Invoke();
+            }
+        }
+
+        void OnDestroy()
+        {
+            DestroyEvent?.Invoke();
+            DestroyEvent = null;
+            UpdateEvent = null;
+        }
+
+        public void AddUpdateListener(UnityAction func)
+        {
+            UpdateEvent += func;
+        }
+        public void RemoveUpdateListener(UnityAction func)
+        {
+            UpdateEvent -= func;
+        }
+
+        public void AddDestroyListener(UnityAction func)
+        {
+            DestroyEvent += func;
+        }
+
+        public void RemoveDestroyListener(UnityAction func)
+        {
+            DestroyEvent -= func;
+        }
+
+        public new Coroutine StartCoroutine(IEnumerator routine)
+        {
+            return _mono.StartCoroutine(routine);
+        }
+
+        public new Coroutine StartCoroutine(string methodName, [DefaultValue("null")] object value)
+        {
+            return _mono.StartCoroutine(methodName, value);
+        }
+        public new Coroutine StartCoroutine(string methodName)
+        {
+            return _mono.StartCoroutine(methodName);
+        }
+
+
+    }
+}
