9982db47964d378d2bfb81a8d5af1747e2c098b9
祭零小白 <uiojkl3588@qq.com>
2023-09-20 00:07:34 +0800

    playerInput

--- Assets/Scripts/Input/PlayerInputMgr.cs
+++ Assets/Scripts/Input/PlayerInputMgr.cs
@@ -5,7 +5,25 @@ using UnityEngine;
 
 public class PlayerInputMgr : Singleton<PlayerInputMgr>
 {
+    private PlayerInputController _inputController;
+    
     private PlayerInputMgr(){}
+
+    public void Init()
+    {
+        _inputController ??= new PlayerInputController();
+        MonoMgr.Instance.AddUpdateListener(UpdateJumpValue);
+    }
+
+    private float _playerJump;
+
+    public Vector2 PlayerMovement => _inputController.PlayerNormal.Move.ReadValue<Vector2>();
     
-    
+    public float PlayerJump;
+
+    private static void UpdateJumpValue()
+    {
+        
+    }
+
 }
