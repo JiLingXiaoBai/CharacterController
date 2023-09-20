4faec942387cdf6c39e021f4b1e734380e8e1892
祭零小白 <uiojkl3588@qq.com>
2023-09-20 20:49:53 +0800

    导入插件

---
+++ Assets/Plugins/JLXB/Framework/Editor/AssetProcessSetting.cs
@@ -0,0 +1,17 @@
+using UnityEditor;
+
+namespace JLXB.Framework.Editor
+{
+    public class AssetProcessSetting : AssetPostprocessor
+    {
+        private void OnPreprocessTexture()
+        {
+            var textureImporter = (TextureImporter)assetImporter;
+            if (!assetPath.Contains("Assets/Resources/Sprites")) return;
+            textureImporter.textureType = TextureImporterType.Sprite;
+            textureImporter.textureShape = TextureImporterShape.Texture2D;
+            textureImporter.mipmapEnabled = false;
+            textureImporter.isReadable = true;
+        }
+    }
+}
