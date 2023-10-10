e46d0753956f8cfd94bc2e43291c598d10ffe49e
JiLingXiaoBai <uiojkl3588@qq.com>
2023-10-10 18:44:00 +0800

    camera 遮挡透明相关

--- Assets/Scripts/Camera/CameraController.cs
+++ Assets/Scripts/Camera/CameraController.cs
@@ -1,12 +1,30 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
     public Transform cameraTarget;
     public LayerMask obstacleLayer;
-    private RaycastHit[] _raycastHits;
-    
+    public Material ditherMaterial;
+    private RaycastHit[] _obstacleHits;
+    private RaycastHit _playerHit;
+    private Dictionary<MeshRenderer, DitherObj> _ditherObjs;
+    private class DitherObj
+    {
+        public Material[] Materials;
+        public bool Flag;
+        public DitherObj(MeshRenderer renderer, bool flag)
+        {
+            Materials = new Material[renderer.materials.Length];
+            for (var i = 0; i < renderer.materials.Length; i++)
+            {
+                Materials[i] = renderer.materials[i];
+            }
+            Flag = flag;
+        }
+    }
 
     private void Awake()
     {
@@ -20,20 +38,61 @@ public class CameraController : MonoBehaviour
         cameraTarget.rotation = cameraRotation;
     }
     
-    private void Update()
-    {
-        
-    }
-
-
+    
     private void UpdateRaycastHit()
     {
         var cameraPos = transform.position;
         var targetPos = cameraTarget.position;
         var viewDir = (targetPos - cameraPos).normalized;
-        var distance = Vector3.Distance(cameraPos, targetPos);
 
-        var ray = new Ray(cameraPos, viewDir);
+        foreach (var ditherObj in _ditherObjs)
+        {
+            ditherObj.Value.Flag = false;
+        }
+        
+        if (!Physics.Raycast(cameraPos, viewDir, out _playerHit)) return;
+        if (!_playerHit.transform.CompareTag("Player"))
+        {
+            var distance = Vector3.Distance(cameraPos, targetPos);
+            Physics.RaycastNonAlloc(cameraPos, viewDir, _obstacleHits, distance, obstacleLayer);
+            foreach (var hit in _obstacleHits.Where(hit => !hit.transform.CompareTag("Player")))
+            {
+                var renderers = hit.transform.GetComponentsInChildren<MeshRenderer>();
+                foreach (var renderer in renderers)
+                {
+                    if (!_ditherObjs.ContainsKey(renderer))
+                    {
+                        var ditherObj = new DitherObj(renderer, true);
+                        _ditherObjs.Add(renderer, ditherObj);
+                        
+                        var tempMaterials = new Material[renderer.materials.Length];
+                        for (var i = 0; i < renderer.materials.Length; i++)
+                        {
+                            tempMaterials[i] = ditherMaterial;
+                        }
+                        renderer.materials = tempMaterials;
+                    }
+                    else
+                    {
+                        _ditherObjs[renderer].Flag = true;
+                    }
+                }
+            }
+        }
+
+        var removeDitherObjs = new List<MeshRenderer>();
+        foreach (var ditherObj in _ditherObjs.Where(ditherObj => ditherObj.Value.Flag == false))
+        {
+            ditherObj.Key.materials = ditherObj.Value.Materials;
+            removeDitherObjs.Add(ditherObj.Key);
+        }
+
+        foreach (var removeDitherObj in removeDitherObjs)
+        {
+            _ditherObjs.Remove(removeDitherObj);
+        }
         
     }
+
+    
 }
