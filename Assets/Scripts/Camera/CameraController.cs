60ef38ea048dee5339a376c393489759ab537f4a
JiLingXiaoBai <uiojkl3588@qq.com>
2023-10-30 18:45:29 +0800

    CameraModel 修改

--- Assets/Scripts/Camera/CameraController.cs
+++ Assets/Scripts/Camera/CameraController.cs
@@ -29,28 +29,42 @@ namespace ARPG.Camera
             return CharacterArchitecture.Interface;
         }
 
+        private ICameraModel _model;
+
         private StateMachine<CameraState, string> _cameraStateMachine;
-        private bool _isLockOn;
-        private bool _isClosingUp;
 
         private void Start()
         {
+            _model = this.GetModel<ICameraModel>();
             _cameraStateMachine = new StateMachine<CameraState, string>();
             _cameraStateMachine.AddState(CameraState.Follow, onEnter: state => { Debug.Log("Follow"); });
             _cameraStateMachine.AddState(CameraState.LockOn, onEnter: state => { Debug.Log("LockOn"); });
             _cameraStateMachine.AddState(CameraState.CloseUp, onEnter: state => { Debug.Log("CloseUp"); });
             _cameraStateMachine.SetStartState(CameraState.Follow);
-            _cameraStateMachine.AddTwoWayTransition(CameraState.Follow, CameraState.LockOn, t => _isLockOn);
-            _cameraStateMachine.AddTransition(CameraState.Follow, CameraState.CloseUp, t => !_isLockOn && _isClosingUp);
-            _cameraStateMachine.AddTransition(CameraState.LockOn, CameraState.CloseUp, t => _isLockOn && _isClosingUp);
-            _cameraStateMachine.AddTransition(CameraState.CloseUp, CameraState.Follow, t => !_isLockOn && !_isClosingUp);
-            _cameraStateMachine.AddTransition(CameraState.CloseUp, CameraState.LockOn, t => _isLockOn && !_isClosingUp);
+            _cameraStateMachine.AddTwoWayTriggerTransition("CameraStateChange", CameraState.Follow, CameraState.LockOn, t => _model.IsLockOn.Value);
+            _cameraStateMachine.AddTriggerTransition("CameraStateChange",CameraState.Follow, CameraState.CloseUp, t => !_model.IsLockOn.Value && _model.IsClosingUp.Value);
+            _cameraStateMachine.AddTriggerTransition("CameraStateChange",CameraState.LockOn, CameraState.CloseUp, t => _model.IsLockOn.Value && _model.IsClosingUp.Value);
+            _cameraStateMachine.AddTriggerTransition("CameraStateChange",CameraState.CloseUp, CameraState.Follow, t => !_model.IsLockOn.Value && !_model.IsClosingUp.Value);
+            _cameraStateMachine.AddTriggerTransition("CameraStateChange",CameraState.CloseUp, CameraState.LockOn, t => _model.IsLockOn.Value && !_model.IsClosingUp.Value);
             _cameraStateMachine.Init();
+            
+            _model.IsLockOn.RegisterWithInitValue(newCount =>
+            {
+                _cameraStateMachine.Trigger("CameraStateChange");
+
+            }).UnRegisterWhenGameObjectDestroyed(gameObject);
+            _model.IsClosingUp.RegisterWithInitValue(newCount =>
+            {
+                _cameraStateMachine.Trigger("CameraStateChange");
+
+            }).UnRegisterWhenGameObjectDestroyed(gameObject);
+            
         }
 
         private void Update()
         {
             _cameraStateMachine.OnLogic();
+            
         }
     }
 }
